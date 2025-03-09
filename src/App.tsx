ec90a4ef2bc05a6960e1107f46b4c4ac62f8fb0b
SamyMtz <samy_mtz15@hotmail.com>
2025-03-08 21:15:28 -0600

    IN-185 Adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Home from './components/Home'
 import useHeaderGif from './hooks/useHeaderGift';
+import mainBanner from './hooks/mainBanner';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   const headerGif = useHeaderGif();
+  const banner = mainBanner();
 
   return (
     <div className="relative w-full h-screen">
@@ -18,7 +21,16 @@ function App() {
       ></div>
       {/* Contenido principal */}
       <div className="relative">
-        <Home />
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={banner}>
+            <Route index element={<Home/>}/>
+            <Route path='wiki' element={'#'}/>
+            <Route path='estadisticas' element={'#'}/>
+            <Route path='acerca' element={'#'}/>
+          </Route>
+        </Routes>
+        </BrowserRouter>
       </div>
     </div>
   )
--- src/hooks/mainBanner.tsx
+++ src/hooks/mainBanner.tsx
@@ -1,40 +1,44 @@
 import naicaLogo from "../assets/images/naicaLogo.png";
+import { Link, Outlet } from "react-router-dom";
 
 const mainBanner = () => {
   return (
+    <>
     <div className="bg-black sticky top-0 z-30">
       <nav className="mx-auto flex items-center justify-between p-6 lg:px-8">
         <div className="flex">
-          <a href="#" className="-m-1 p-1.5">
+          <Link to="/" className="-m-1 p-1.5">
             <img
               alt="../components/Home.tsx"
               src={naicaLogo}
               className="h-12 w-auto"
             />
-          </a>
+          </Link>
         </div>
         <div className="flex gap-x-12">
-              <a
-                href="#"
+              <Link
+                to="/wiki"
                 className="text-sm/6 font-semibold"
               >
                 Wiki
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                to="/estadisticas"
                 className="text-sm/6 font-semibold"
               >
                 Estadísticas
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                to="/acerca"
                 className="text-sm/6 font-semibold"
               >
                 Acerca de
-              </a>
+              </Link>
           </div>
       </nav>
     </div>
+    <Outlet/>
+    </>
   );
 };
 
